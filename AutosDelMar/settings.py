b32801c173253ee188716991d92deecaa46feb40
Angel <jos.poncea@duocuc.cl>
2024-06-29 22:06:17 -0400

    Inicio redireccionado

--- AutosDelMar/settings.py
+++ AutosDelMar/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'autos',
-    'clientes'
+    'clientes',
 ]
 
 MIDDLEWARE = [
--- AutosDelMar/urls.py
+++ AutosDelMar/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autos/',include('autos.urls')),
     path('clientes/', include('clientes.urls')),
+    path('Inicio', RedirectView.as_view(url='/autos/', permanent=True)),
+
 
 ]
 
