924bda1779a10d49424e0c5d3f460f3cf918f2c0
Angel <jos.poncea@duocuc.cl>
2024-06-30 01:42:03 -0400

    Registro de usuario desde admin

--- AutosDelMar/settings.py
+++ AutosDelMar/settings.py
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #urls para archivos media de base de datos
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- clientes/models.py
+++ clientes/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    Correo = models.CharField(primary_key=True, max_length=10)
+    Correo = models.EmailField(primary_key=True, max_length=10)
     Nombre = models.CharField(max_length=20, null=False)
     Clave = models.CharField(max_length=20, null=False)
     def __str__(self):
-        return str("Cliente: "+str(self.Nombre) )
+        return self.Nombre
