5a8a3c56fe27497278427a34fcd0aee454addf99
Angel <jos.poncea@duocuc.cl>
2024-06-30 00:30:50 -0400

    Clientes Actualizado

--- clientes/models.py
+++ clientes/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    rut_cli = models.CharField(primary_key=True, max_length=10)
-    nom_cli = models.CharField(max_length=20, null=False)
-    appa_cli = models.CharField(max_length=20, null=False)
-    apma_cli = models.CharField(max_length=20, null=False)
+    Correo = models.CharField(primary_key=True, max_length=10)
+    Nombre = models.CharField(max_length=20, null=False)
+    Clave = models.CharField(max_length=20, null=False)
+    def __str__(self):
+        return str("Cliente: "+str(self.Nombre) )
