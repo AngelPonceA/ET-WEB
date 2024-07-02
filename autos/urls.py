492be7322eb405669e7d2a7a80e38a4c04a4e49f
Angel <jos.poncea@duocuc.cl>
2024-07-02 03:25:34 -0400

    barra de busqueda funcional

--- autos/urls.py
+++ autos/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name = 'home'),
+    path('filtrar/',views.filtrar, name = 'filtrar'),
+
 ]
--- autos/views.py
+++ autos/views.py
@@ -8,5 +8,12 @@ def home(request):
     context = {
         'autos': autos,
     }
-    return render(request,'autos/home.html',context)
+    return render(request,'autos/home.html', context)
 
+def filtrar(request):
+    query = request.GET.get('query', '')
+    if query:
+        autos = auto.objects.filter(modelo__icontains=query)
+    else:
+        autos = auto.objects.all()
+    return render(request, 'autos/home.html', {'autos': autos, 'query': query})
